c6ff030830a4a1e9b407d9fd6a926870f8395745
Marcello <marcello.mercanzin@gmail.com>
2024-02-23 17:58:28 +0100

    nav bar js

---
+++ UNIT-2/Settimana-1/ProgettoSettimanale-u2-s1/assets/js/script.js
@@ -0,0 +1,12 @@
+const header = document.getElementById("header");
+const linkStarted = document.getElementById("linkStarted");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 530) {
+    header.style.background = "white";
+    linkStarted.style.backgroundColor = "#1a8917";
+  } else {
+    header.style.background = "#ffc017";
+    linkStarted.style.backgroundColor = "#191919";
+  }
+});
