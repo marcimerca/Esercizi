4c14734821a46fd73822daf4f51a5bc19102075e
Marcello <marcello.mercanzin@gmail.com>
2024-02-23 21:31:05 +0100

    sistemato js

--- UNIT-2/Settimana-1/ProgettoSettimanale-u2-s1/assets/js/script.js
+++ UNIT-2/Settimana-1/ProgettoSettimanale-u2-s1/assets/js/script.js
@@ -2,30 +2,28 @@ const header = document.getElementById("header");
 const linkStarted = document.getElementById("linkStarted");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 450) {
-    header.style.background = "white";
-    linkStarted.style.backgroundColor = "#1a8917";
-  } else {
-    header.style.background = "#ffc017";
-    linkStarted.style.backgroundColor = "#191919";
-  }
-});
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 450) {
-    header.style.background = "white";
-    linkStarted.style.backgroundColor = "#1a8917";
-  } else {
-    header.style.background = "#ffc017";
-    linkStarted.style.backgroundColor = "#191919";
-  }
-
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    if (window.scrollY > 310) {
-      header.style.background = "white";
+  if (window.matchMedia("(max-width: 576px)").matches) {
+    if (window.scrollY > 290) {
+      header.style.backgroundColor = "white";
       linkStarted.style.backgroundColor = "#1a8917";
     } else {
-      header.style.background = "#ffc017";
+      header.style.backgroundColor = "#ffc017";
+      linkStarted.style.backgroundColor = "#191919";
+    }
+  } else if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.scrollY > 295) {
+      header.style.backgroundColor = "white";
+      linkStarted.style.backgroundColor = "#1a8917";
+    } else {
+      header.style.backgroundColor = "#ffc017";
+      linkStarted.style.backgroundColor = "#191919";
+    }
+  } else {
+    if (window.scrollY > 450) {
+      header.style.backgroundColor = "white";
+      linkStarted.style.backgroundColor = "#1a8917";
+    } else {
+      header.style.backgroundColor = "#ffc017";
       linkStarted.style.backgroundColor = "#191919";
     }
   }
