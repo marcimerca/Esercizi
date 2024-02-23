8fd64e30daacbbf3307c05bec4c078269a6335e3
Marcello <marcello.mercanzin@gmail.com>
2024-02-23 20:18:07 +0100

    aggiunto script js per tablet

--- UNIT-2/Settimana-1/ProgettoSettimanale-u2-s1/assets/js/script.js
+++ UNIT-2/Settimana-1/ProgettoSettimanale-u2-s1/assets/js/script.js
@@ -10,3 +10,23 @@ window.addEventListener("scroll", function () {
     linkStarted.style.backgroundColor = "#191919";
   }
 });
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 450) {
+    header.style.background = "white";
+    linkStarted.style.backgroundColor = "#1a8917";
+  } else {
+    header.style.background = "#ffc017";
+    linkStarted.style.backgroundColor = "#191919";
+  }
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.scrollY > 310) {
+      header.style.background = "white";
+      linkStarted.style.backgroundColor = "#1a8917";
+    } else {
+      header.style.background = "#ffc017";
+      linkStarted.style.backgroundColor = "#191919";
+    }
+  }
+});
