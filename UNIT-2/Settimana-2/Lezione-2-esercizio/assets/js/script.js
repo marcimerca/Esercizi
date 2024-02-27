bd021b7dbefd975c4eb8e271d5d93f499550580f
Marcello <marcello.mercanzin@gmail.com>
2024-02-27 19:58:39 +0100

    aggiunto alcune funzioni

---
+++ UNIT-2/Settimana-2/Lezione-2-esercizio/assets/js/script.js
@@ -0,0 +1,19 @@
+const btnCard = document.querySelectorAll(".button-mostra-nascondi");
+
+btnCard.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (this.innerText === "Nascondi sezione") {
+      this.innerText = "Mostra";
+    } else {
+      this.innerText = "Nascondi sezione";
+    }
+  });
+});
+
+// contare numero viaggi
+
+window.onload = function () {
+  Viaggi = document.querySelectorAll(".card");
+  const numViaggi = Viaggi.length;
+  alert(`In questa pagina sono presenti ${numViaggi} viaggi per te!`);
+};
