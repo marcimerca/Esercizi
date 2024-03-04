cb6fc702ad12660b9382711f8c3e7f121ec2483c
Marcello <marcello.mercanzin@gmail.com>
2024-03-04 17:04:38 +0100

    esercizio unit 2 settimana 3 lezione 1 - classi

---
+++ UNIT-2/Settimana-3/Giorno1/lezione/assets/js/script.js
@@ -0,0 +1,63 @@
+// esercizio 1
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+  confronta(altroUtente) {
+    if (this.age > altroUtente.age) {
+      console.log(
+        `${this.firstName} ha ${this.age} anni ed è più vecchio di ${altroUtente.firstName}, che ha ${altroUtente.age} anni. `
+      );
+    } else {
+      console.log(
+        `${altroUtente.firstName} ha ${altroUtente.age} anni ed è più vecchio di ${this.firstName}, che ha ${this.age} anni.`
+      );
+    }
+  }
+}
+const user1 = new User("Marcello", "Mercanzin", 27, "Vicenza");
+console.log(user1);
+const user2 = new User("Zlatan", "Ibrahimovic", 42, "Milano");
+const user3 = new User("Brad", "Pitt", 60, "USA");
+user1.confronta(user2);
+user2.confronta(user3);
+
+// esercizio 2
+
+class Pet {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
+  confrontaPadrone(altroPet) {
+    if (this.ownerName == altroPet.ownerName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const listaPets = document.getElementById("listaPets");
+const petName = document.getElementById("petName");
+const ownerName = document.getElementById("ownerName");
+const species = document.getElementById("species");
+const breed = document.getElementById("breed");
+const btnAggiungi = document.getElementById("aggiungiPet");
+
+btnAggiungi.addEventListener("click", function () {
+  const newPet = new Pet(
+    petName.value,
+    ownerName.value,
+    species.value,
+    breed.value
+  );
+  let listItem = document.createElement("li");
+  listItem.innerText = `Pet Name: ${newPet.petName}, Owner Name: ${newPet.ownerName}, Species: ${newPet.species}, Breed: ${newPet.breed}`;
+  listaPets.appendChild(listItem);
+});
